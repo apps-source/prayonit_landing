beae9898ec932b31c45e009b395353e822d1e08f
David Tischler <davidtischler@Davids-Mac-Studio.local>
2026-07-13 18:07:05 -0400

    Add Google Analytics 4

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Analytics } from "@vercel/analytics/next";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -33,6 +34,7 @@ export default function RootLayout({
         {children}
         <Analytics />
       </body>
+      <GoogleAnalytics gaId="G-9QC1R53Q8N" />
     </html>
   );
 }
