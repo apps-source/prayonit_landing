ca8b329cee91782a5474ec154e54e01fab01e42a
David Tischler <davidtischler@Davids-Mac-Studio.local>
2026-07-13 14:00:34 -0400

    Fix appLinks build error

--- src/data/appLinks.ts
+++ src/data/appLinks.ts
@@ -3,4 +3,3 @@
 export const APP_STORE_URL = "https://apps.apple.com/us/app/prayonit/id6754224870";
 export const GOOGLE_PLAY_URL =
   "https://play.google.com/store/apps/details?id=com.nexwavestudiosapp.prayonit";
-exit
